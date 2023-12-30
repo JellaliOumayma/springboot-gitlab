fb3f72361cbc8171ef2f1b930d50a4ee3fdeac0f
JellaliOumayma <124064365+JellaliOumayma@users.noreply.github.com>
2023-12-30 08:42:40 +0100

    Update employee.service.js

--- my-app/src/services/employee.service.js
+++ my-app/src/services/employee.service.js
@@ -1,22 +1,25 @@
 import httpClient from "../http-common";
 
-const getAll = () => {
+const employeeService = {
+  getAll: () => {
     return httpClient.get('/employees');
-}
-
-const create = data => {
+  },
+  
+  create: data => {
     return httpClient.post("/employees", data);
-}
+  },
 
-const get = id => {
+  get: id => {
     return httpClient.get(`/employees/${id}`);
-}
+  },
 
-const update = data => {
+  update: data => {
     return httpClient.put('/employees', data);
-}
+  },
 
-const remove = id => {
+  remove: id => {
     return httpClient.delete(`/employees/${id}`);
-}
-export default { getAll, create, get, update, remove };
+  }
+};
+
+export default employeeService;
